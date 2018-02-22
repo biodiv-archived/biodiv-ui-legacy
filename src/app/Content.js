4d570832b8fe85adf8f37667006cdca132a1a228
Sunil Yadav <yadavsunil00001@gmail.com>
2018-02-22 11:48:53 +0530

    autocomplete done

--- src/app/Content.js
+++ src/app/Content.js
@@ -5,6 +5,7 @@ import ObservationListContainer from '../observation/ObservationListContainer';
 import {fetchUserGroupList,fetchSpeciesGroup,fetchLanguages} from '../actions/index';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
+import style from './style.css';
 
 class Content extends Component {
 
