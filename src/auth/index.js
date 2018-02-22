34abf2c0467c549ae8d470bd410f826db503269c
pearlsravanthi <sravanthi@strandls.com>
2018-02-22 19:04:33 +0530

    REFS : removed google maps api and renamed forgetPassword

--- src/auth/index.js
+++ src/auth/index.js
@@ -2,7 +2,7 @@ import Login from './Login';
 import Logout from './Logout';
 import AuthUtils from './AuthUtils';
 import Register from './Register';
-import ForgetPassword from './ForgetPassword';
+import ForgotPassword from './ForgotPassword';
 import ResetPassword from './ResetPassword';
 
-export {Login, Logout,AuthUtils,Register,ForgetPassword,ResetPassword}
+export {Login, Logout,AuthUtils,Register,ForgotPassword,ResetPassword}
