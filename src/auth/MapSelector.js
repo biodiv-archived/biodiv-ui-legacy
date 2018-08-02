180f89027581598d5dabc7033803caa1ec6bc3b1
Priyanshu Agarwal <34156611+priyanshu-a@users.noreply.github.com>
2018-08-02 13:19:26 +0530

    correcting zoom level

--- src/auth/MapSelector.js
+++ src/auth/MapSelector.js
@@ -8,8 +8,8 @@ export default function () {
     GoogleMapsLoader.load(function(google) {
 	var mapDiv = document.getElementById('gmap')
     map = new google.maps.Map(mapDiv, {
-        center: {lat: 27.478, lng: 90.552}, // Bangalore
-        zoom: 7.5
+        center: {lat: 27.478, lng: 90.552}, // Bhutan
+        zoom: 7
       });
     var input = document.getElementById('pac-input');
 
